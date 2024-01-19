cc6f032d33e91bc46250064b9e6bdb327d16b67c
Thinh <thinh1198@hotmail.com>
2024-01-19 21:52:12 +0000

    code to dynamic change text block colours

--- static/script.js
+++ static/script.js
@@ -1,13 +1,25 @@
 
 $(document).ready(() => {
-    currentTime();
+    var time = currTime();
+    colorTimeBlock(12);
+
 })
 
-function currentTime() {
+function currTime() {
     var dateEl = $('#currentDay');
     var createTimeEl = $('<div>');
-    var currentDate = dayjs().format('[Current day: ]dddd[,] MMMM d[th]');
-    dateEl.append(createTimeEl.text(currentDate));
+    var currDate = dayjs().format('[Current day: ]dddd[,] MMMM d[th]');
+    dateEl.append(createTimeEl.text(currDate));
+    return parseInt(dayjs().format('H'));
 }
 
+function colorTimeBlock(currTime) {
+    $('.description').each(function() {
+        var blockTime = ($(this).attr('data-time'))
+
+        blockTime < currTime ? $(this).css('background-color', 'grey') 
+        : blockTime > currTime ? $(this).css('background-color', 'green')
+        : $(this).css('background-color', 'red');
+    })
+}
 
