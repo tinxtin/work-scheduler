e8ae44e5465b2396ccd2b2e78f7d1128d54c2322
Thinh <thinh1198@hotmail.com>
2024-01-22 10:47:55 +0000

    add code to make textbox editable

--- static/script.js
+++ static/script.js
@@ -1,25 +1,32 @@
 
 $(document).ready(() => {
     var time = currTime();
-    colorTimeBlock(12);
+    colorTimeBlock(time);
 
+    $('.description').on('click', function() {
+        var $descEl = $(this);
+        isEditable = $descEl.is('.editable');
+        $descEl.prop('contentEditable',!isEditable).toggleClass('editable')
+        if (!$descEl.is(':focus')) {$(this).focus();}
+        $descEl.on('focusout', function() {
+            console.log('test')
+        });
+    })
 })
 
 function currTime() {
-    var dateEl = $('#currentDay');
-    var createTimeEl = $('<div>');
+    var $dateEl = $('#currentDay');
+    var $createTimeEl = $('<div>');
     var currDate = dayjs().format('[Current day: ]dddd[,] MMMM d[th]');
-    dateEl.append(createTimeEl.text(currDate));
+    $dateEl.append($createTimeEl.text(currDate));
     return parseInt(dayjs().format('H'));
 }
 
 function colorTimeBlock(currTime) {
     $('.description').each(function() {
-        var blockTime = ($(this).attr('data-time'))
-
-        blockTime < currTime ? $(this).css('background-color', 'grey') 
-        : blockTime > currTime ? $(this).css('background-color', 'green')
+        var $blockTime = ($(this).attr('data-time'));
+        $blockTime < currTime ? $(this).css('background-color', 'grey') 
+        : $blockTime > currTime ? $(this).css('background-color', 'green')
         : $(this).css('background-color', 'red');
     })
 }
-
